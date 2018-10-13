ec2c4811b520280558ad9e33f567af484c4c38b4
ardalansajedi94 <sajad.sajedi94@gmail.com>
2018-10-13 13:46:17 +0330

    done voucher

---
+++ app/src/main/java/ir/hotelairport/androidapp/airportHotels/Auth/MyProfileFragment.java
@@ -0,0 +1,23 @@
+package ir.hotelairport.androidapp.airportHotels.Auth;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyProfileFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+
+}
